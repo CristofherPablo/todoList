81f7a94f1bc4639120499785cca9059305377ccc
Cristofher Pablo <chatcomchris@gmail.com>
2023-05-04 23:36:29 -0300

    Fix: adding another container to wrap the page

--- src/js/buildPage.js
+++ src/js/buildPage.js
@@ -1,33 +1,43 @@
 // import icon image
-import noteIcon from '../assets/lista.png';
+import listaIcon from '../assets/lista.png';
+import noteIcon from '../assets/greyNote.png';
 
 class PageLayout {
   constructor() {
     this.body = document.querySelector('body');
+    this.container = document.createElement('div');
     this.iconLink = document.querySelector(`link[rel~='icon']`);
   }
 
   init() {
     this.renderIcon();
+    this.renderContainer();
+  }
+
+  //get the icon image and render in the page
+  renderIcon() {
+    this.iconLink.href = listaIcon;
+  }
+
+  renderContainer() {
+    this.container.classList.add('container');
+    this.body.appendChild(this.container);
     this.renderHeader();
     this.renderMainSection();
     this.renderFooter();
   }
 
-  //get the icon image and render in the page
-  renderIcon() {
-    this.iconLink.href = noteIcon;
-  }
-
   renderHeader() {
     const header = `
     <header>
-     <div class="logo"></div>
+     <div class="logo">
+     <img src=${noteIcon} alt="icon of a paper sheet simulating a to-do list"> To-do List
+     </div>
      <div class="darkLightMode">
-       <button></button>
+       <button>Temporary</button>
      </div>
     </header>`;
-    this.body.insertAdjacentHTML('beforeend', header);
+    this.container.insertAdjacentHTML('beforeend', header);
   }
 
   renderMainSection() {
@@ -35,12 +45,21 @@ class PageLayout {
        <div class="sideBar"></div>
        <div class="content"></div>
      </section>`;
-    this.body.insertAdjacentHTML('beforeend', main);
+    this.container.insertAdjacentHTML('beforeend', main);
   }
 
   renderFooter() {
-    const footer = `<footer><p></p></footer>`;
-    this.body.insertAdjacentHTML('beforeend', footer);
+    const footer = `<footer><p>
+    Made by
+    <a href="https://github.com/CristofherPablo" target="_blank"
+      >Cristofher Guersoni</a
+    >
+    for
+    <a href="https://www.theodinproject.com/paths" target="_blank"
+      >Odin Project</a
+    >
+    </p></footer>`;
+    this.container.insertAdjacentHTML('beforeend', footer);
   }
 }
 
