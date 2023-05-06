38af389eebde332e88b80b14e1b7cfe421908810
Cristofher Pablo <chatcomchris@gmail.com>
2023-05-06 14:29:17 -0300

    Feat: create the dark/light change transitions

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -7,16 +7,22 @@ class PageLogic {
     this.bodyEventDelegation();
   }
 
-  bodyEventDelegation(){
-    this.body.addEventListener('click',(event) => {
-      if(event.target.classList.contains('theme')){
-        this.toggleThemeBtn();
+  bodyEventDelegation() {
+    this.body.addEventListener('click', (event) => {
+      if (event.target.classList.contains('theme')) {
+        let themeBtn = event.target;
+        this.toggleThemeBtn(themeBtn);
       }
-    })
+    });
   }
 
-  toggleThemeBtn() {
-    console.log('test 1');
+  toggleThemeBtn(themeBtn) {
+    this.body.classList.toggle('light-mode');
+    if (themeBtn.innerText === 'Light Mode') {
+      themeBtn.innerText = 'Dark Mode';
+    } else {
+      themeBtn.innerText = 'Light Mode';
+    }
   }
 }
 const pageLogic = new PageLogic();
